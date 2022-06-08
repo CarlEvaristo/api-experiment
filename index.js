238436b4414bfdd70bdabd8af6f24127cc9ff9ad
CarlEverest78 <thepythoneer@gmail.com>
2022-06-08 09:21:37 +0200

    final commit

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ Challenge:
 3. MANUALLY copy over the fetch, but using the URL below 👇
     https://dog.ceo/api/breeds/image/random
 */
-let dogImgEl = document.getElementById("dogImg")
+let imgEl = document.getElementById("imgWrapper")
 
 fetch("https://dog.ceo/api/breeds/image/random")
     .then(response => response.json())
-    .then(data => dogImgEl.src=data.message)
+    .then(data => {
+        imgEl.innerHTML = `<img src=${data.message} alt="random dow image" class="dog-img">`
+    })
 
 
 
